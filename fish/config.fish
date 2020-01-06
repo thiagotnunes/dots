8c107bbe8e884e5c83df32a5be12333a72584ce8
Thiago Nunes <tnunes@atlassian.com>
2020-01-07 10:54:50 +1100

    Updates fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 . "$HOME/.dots/fish/custom/path"
 . "$HOME/.dots/fish/custom/env"
 . "$HOME/.dots/fish/custom/aliases"
+. "$HOME/.dots/fish/custom/prompt"
+. "$HOME/.dots-local/fish/custom/aliases"
 
