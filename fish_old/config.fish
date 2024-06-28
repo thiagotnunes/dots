a55eb3cdbf0116d8c4eeade7c79600ba933aaa23
Thiago Tasca Nunes <thiagotnunes@google.com>
2024-06-28 10:27:47 +1000

    cleanup

--- fish_old/config.fish
+++
@@ -1,6 +0,0 @@
-. "$HOME/.dots/fish/custom/path"
-. "$HOME/.dots/fish/custom/env"
-. "$HOME/.dots/fish/custom/aliases"
-. "$HOME/.dots/fish/custom/prompt"
-. "$HOME/.dots-local/fish/custom/aliases"
-
